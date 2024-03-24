a101f1ff087f1a6cff644f46f93b049c691fbf91
jessy <spidx@mbp-de-spidx.home>
2024-03-24 21:02:20 +0100

    fix background

--- packages/FlamencoJS/src/effects/Background.ts
+++ packages/FlamencoJS/src/effects/Background.ts
@@ -6,10 +6,11 @@ export class BackgroundColor extends AbstractSetEffect<BackgroundColorEffect> {
   set(dataArray: Uint8Array): void {
     this.effects.forEach(({elements}) => {
       elements.forEach((element, i) => {
-        const red = extractIndexValue({index: i, data: dataArray, elementNumber: this.effects.length});
+        const red =  dataArray[i * Math.round(128 / this.effects.length)];
         const blue = 255 - red;
         element.style.backgroundColor = `rgb(${red}, 0, ${blue})`;
       });
+
     });
   }
 }
