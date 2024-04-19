bff994756d5d3152d737984353a1f3846d510cc8
Piero NERI <piero.neri@hotmail.fr>
2024-04-19 13:00:37 +0200

    section 1 V1

--- packages/demo/src/components/MainGrid.vue
+++ packages/demo/src/components/MainGrid.vue
@@ -22,10 +22,15 @@ import E_letter from '../assets/svg/e_letter.svg'
 import N_letter from '../assets/svg/n_letter.svg'
 import C_letter from '../assets/svg/c_letter.svg'
 import O_letter from '../assets/svg/o_letter.svg'
-import CONCERT1 from '../assets/img/concert-1.jpeg'
-import CONCERT2 from '../assets/img/concert-2.jpeg'
-import CONCERT3 from '../assets/img/concert-3.jpeg'
-import CONCERT4 from '../assets/img/concert-4.jpeg'
+import NOISE from '../assets/svg/noise.svg'
+import LOGO from '../assets/svg/logo.svg'
+import CONCERT1 from '../assets/img/concert-1.jpg'
+import CONCERT2 from '../assets/img/concert-2.jpg'
+import CONCERT3 from '../assets/img/concert-3.jpg'
+import CONCERT4 from '../assets/img/concert-4.jpg'
+import CONCERT5 from '../assets/img/concert-5.jpg'
+import CONCERT6 from '../assets/img/concert-6.jpg'
+import CONCERT7 from '../assets/img/concert-7.jpg'
 
 const cells = ref<Cell[]>([])
 const screenWidth = ref(window.innerWidth)
@@ -39,28 +44,39 @@ const updateCellSizes = () => {
     ]
 
     const fixedCellParams = [
-        { backgroundColor: themeColor.value[0], contentSlot: 'logo' },
+        { backgroundColor: themeColor.value[0], contentSVG: LOGO },
         { backgroundColor: themeColor.value[2] },
         { backgroundColor: themeColor.value[0] },
         { backgroundColor: themeColor.value[0] },
-        { backgroundColor: themeColor.value[0], contentSlot: 'doc' },
-        { backgroundColor: themeColor.value[0], contentSlot: 'about' },
+        { backgroundColor: themeColor.value[0], contentSlot: '<router-link  to="/doc">docs</router-link>' },
+        { backgroundColor: themeColor.value[0], contentSlot: '<router-link  to="/about">about</router-link>' },
         { backgroundColor: themeColor.value[1] },
         {
-            backgroundColor: themeColor.value[0],
+            backgroundImage: CONCERT4,
             contentSVG: F_letter,
+            noise: NOISE,
+            backgroundPosition: '60% 40%',
+            backgroundSize: '450%',
+            blur: 3
+
         },
         { backgroundColor: themeColor.value[2], contentSVG: LA_letter },
         { backgroundColor: themeColor.value[0], contentSVG: M_letter },
         { backgroundColor: themeColor.value[2], contentSVG: E_letter },
         {
-            backgroundImage: 'https://picsum.photos/200/300',
-            blur: 50,
+            backgroundImage: CONCERT4,
+            backgroundPosition: '35% 50%',
+            backgroundSize: '450%',
+            blur: 3,
+
         },
         { backgroundColor: themeColor.value[2], radius: '0 0 50% 0' },
         {
-            backgroundImage: 'https://picsum.photos/200/300',
-            noise: 50,
+            backgroundImage: CONCERT3,
+            backgroundPosition: '35% 30%',
+            backgroundSize: '450%',
+            noise: NOISE,
+
         },
         { backgroundColor: themeColor.value[0] },
         { backgroundColor: themeColor.value[0] },
@@ -69,12 +85,17 @@ const updateCellSizes = () => {
             taller: 3,
             backgroundPosition: '25% 26%',
             backgroundSize: 'cover',
+            noise: NOISE,
         },
         { backgroundColor: themeColor.value[2], contentSVG: N_letter },
         { backgroundColor: themeColor.value[0], contentSVG: C_letter },
         {
-            backgroundImage: 'https://picsum.photos/200/300',
-            blur: 50,
+            backgroundImage: CONCERT7,
+            blur: 3,
+            backgroundPosition: '40% 50%',
+            backgroundSize: '300%',
+
+
         },
         { backgroundColor: themeColor.value[0] },
         { backgroundColor: themeColor.value[0] },
@@ -83,13 +104,18 @@ const updateCellSizes = () => {
         { backgroundColor: themeColor.value[0] },
         { backgroundColor: themeColor.value[2], radius: '0 50% 0 0' },
         {
-            backgroundImage: 'https://picsum.photos/200/300',
-            blur: 50,
+            backgroundImage: CONCERT5,
+            backgroundPosition: '45% 50%',
+            backgroundSize: '300%',
         },
         { backgroundColor: themeColor.value[0] },
         {
-            backgroundImage: 'https://picsum.photos/200/300',
-            noise: 50,
+            backgroundImage: CONCERT6,
+            backgroundPosition: '85% 50%',
+            backgroundSize: '300%',
+            taller: 2,
+            blur: 2
+
         },
         { backgroundColor: themeColor.value[0] },
         { backgroundColor: themeColor.value[0] },
@@ -104,12 +130,8 @@ const updateCellSizes = () => {
                 width: cellSize + 'px',
                 height: cellSize + 'px',
                 backgroundColor: fixedCellParam.backgroundColor || 'none',
-                blurEffect: fixedCellParam.blur
-                    ? `blur(${fixedCellParam.blur}px)`
-                    : 'none',
-                noiseEffect: fixedCellParam.noise
-                    ? 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/w8AAwAB/7+o9gAAAABJRU5ErkJggg==")'
-                    : 'none',
+                blurEffect: fixedCellParam.blur,
+                noiseEffect: fixedCellParam.noise,
                 backgroundImage: fixedCellParam.backgroundImage,
                 transform: fixedCellParam.transform,
                 taller: fixedCellParam.taller,
@@ -121,20 +143,16 @@ const updateCellSizes = () => {
                 radius: fixedCellParam.radius || '0',
             }
         } else {
+            const random = Math.random()
             return {
                 width: cellSize + 'px',
                 height: cellSize + 'px',
-                backgroundColor: 'red',
-                blurEffect: Math.random() > 0.5 ? 'blur(5px)' : 'none',
-                noiseEffect:
-                    Math.random() > 0.5
-                        ? 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/w8AAwAB/7+o9gAAAABJRU5ErkJggg==")'
-                        : 'none',
-                backgroundImage: null,
-                contentSlot: Math.random() > 0.5 ? 'Random Content' : '',
-
-                contentSVG: '',
-                contentText: '',
+                backgroundColor: themeColor.value[Math.floor(Math.random() * themeColor.value.length)],
+                blurEffect: random > 0.7 ? 3 : 0,
+                noiseEffect: random < 0.3 ? NOISE : '',
+                backgroundImage: [CONCERT1, CONCERT2, CONCERT3, CONCERT4, CONCERT5, CONCERT6, CONCERT7][Math.floor(Math.random() * 7)],
+                backgroundPosition: `${Math.floor(Math.random() * 60) + 20}% ${Math.floor(Math.random() * 60) + 20}%`,
+                backgroundSize: `${Math.floor(Math.random() * 200) + 200}%`,
             }
         }
     })
@@ -163,9 +181,9 @@ onUnmounted(() => {
 .grid-container {
     display: grid;
     grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(5, 1fr);
+    grid-template-rows: repeat(6, 1fr);
     width: 100vw;
-    height: 100vh;
+    height: calc(100vh + 100vh/4);
     margin: 0;
     padding: 0;
     gap: 0;
--- packages/demo/src/components/MainGridCell.vue
+++ packages/demo/src/components/MainGridCell.vue
@@ -6,6 +6,26 @@
             :src="props.cellData.contentSVG"
             alt="Image"
         />
+      <div
+          :style="blurStyles"
+          v-if="props.cellData.blurEffect"
+        class="grid-cell__blur"
+    </div>
+  <div class="grid-cell__noise"  v-if="props.cellData.noiseEffect">
+    <svg>
+    <filter id="noise">
+      <feTurbulence id="turbulence">
+        <animate
+            attributeName="baseFrequency"
+            dur="50s"
+            values="0.9 0.9;0.8 0.8; 0.9 0.9"
+            repeatCount="indefinite"
+        ></animate>
+      </feTurbulence>
+      <feDisplacementMap in="SourceGraphic" scale="60"></feDisplacementMap>
+    </filter>
+  </svg></div>
+
 <!--        <img
             v-if="props.cellData.backgroundImage"
             :style="imgStyles"
@@ -16,6 +36,7 @@
         <span v-if="props.cellData.contentText">{{
             props.cellData.contentText
         }}</span>
+        <div class="grid-cell__contentSlot" v-else-if="props.cellData.contentSlot" v-html="props.cellData.contentSlot"></div>
     </div>
 </template>
 
@@ -28,8 +49,8 @@ const cellStyles = computed(() => {
     return {
         width: props.cellData.larger ? '100%' : props.cellData.width,
         height: props.cellData.taller ? '100%' : props.cellData.height,
+        background: "url("+props.cellData.backgroundImage+") no-repeat left top",
         backgroundColor: props.cellData.backgroundColor,
-        backgroundImage: 'url('+props.cellData.backgroundImage+')',
         backgroundPosition: props.cellData.backgroundPosition,
         backgroundSize: props.cellData.backgroundSize,
         contentSlot: props.cellData.contentSlot,
@@ -38,13 +59,17 @@ const cellStyles = computed(() => {
             : '',
         borderRadius: props.cellData.radius,
         gridRow: props.cellData.taller ? `span ${props.cellData.taller}` : null,
+
     }
 })
 
-const imgStyles = computed(() => {
+if(props.cellData.noiseEffect){
+  console.log(cellStyles)
+}
+
+const blurStyles = computed(() => {
     return {
-        filter: `blur(${props.cellData.blurEffect}px)`,
-        transform: props.cellData.transform,
+        backdropFilter: `blur(${props.cellData.blurEffect}px)`,
     }
 })
 </script>
@@ -58,12 +83,25 @@ const imgStyles = computed(() => {
     overflow: hidden;
     position: relative;
     background-repeat: no-repeat;
-  filter: grayscale(1);
+    font-family: "League Spartan", sans-serif;
+
+  &__noise {
+    position: absolute;
+    top: -50px;
+    left: -50px;
+    width: calc(100% + 50px);
+    height: calc(100% + 50px);
+    background: repeating-linear-gradient(rgba(17, 17, 17, 0.16), rgba(255, 255, 255, 0.25) 50%, rgb(0, 0, 0) 50%, rgba(0, 0, 0, 0.45));
+    background-size: 5px 5px;
+    filter: url(#noise);
+  }
+
 
     &__svg {
         max-width: 100%;
         max-height: 100%;
         width: 100%;
+        z-index: 1;
     }
 
     &__background {
@@ -72,6 +110,28 @@ const imgStyles = computed(() => {
       object-fit: none;
       position: absolute;
     }
+
+    &__blur {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      z-index: 0;
+    }
+
+    &__contentSlot {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+  router-link {
+    text-decoration: underline;
+    font-size: 1.5rem;
+    text-underline-offset: 0.5em;
+    color: inherit;
+  }
 }
 
 .large-column {
