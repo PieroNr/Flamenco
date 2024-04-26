6c87ba58774348d9e8d5952a7d3639c33447582b
jessy <spidx@MacBook-Pro-de-spidx.local>
2024-04-26 15:39:32 +0200

    update router

--- packages/demo/src/router.ts
+++ packages/demo/src/router.ts
@@ -2,8 +2,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/HomeView.vue'
 import About from './pages/AboutView.vue'
-import TestThree from './pages/TestThree.vue'
-import Test from './pages/Test.vue'
 
 const routes = [
     {
@@ -15,17 +13,7 @@ const routes = [
         path: '/about',
         name: 'About',
         component: About,
-    },
-    {
-        path: '/three',
-        name: 'TestThree',
-        component: TestThree,
-    },
-    {
-        path: '/test',
-        name: 'Test',
-        component: Test,
-    },
+    }
 ]
 
 const router = createRouter({
